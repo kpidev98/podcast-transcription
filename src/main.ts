2b3f9d7dd789bc9b3f5b0d3477aa15272fec60f6
Kostiantyn1303 <kosty13humera@gmail.com>
2025-05-14 23:40:17 +0200

    cors problem

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
